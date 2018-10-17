5d008d0c86c17f310b6f1891fdd209aea50a1d59
mostrozny <ostrozny.michal@gmail.com>
2018-10-17 09:54:19 +0100

    Base

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+
+var gulp = require('gulp');
+
+
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+    return gulp.src('scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'))
+});
+
+// watch
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', ['sass']);
+});
---
+++ js/app.js
@@ -0,0 +1,25 @@
+import Swiper from 'swiper'
+
+document.addEventListener('DOMContentLoaded', () => {
+    //SWIPER
+        const mySwiper = new Swiper('.swiper-container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+            flipEffect: {
+                rotate: 30,
+                slideShadows: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+            },
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + (index + 1) + '</span>';
+            }
+        });
+});
